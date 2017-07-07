5f5453a90423b3cc44974218431a0466ee5cc0e5
Kristen De La Cerda <kndelacerda@gmail.com>
2017-07-06 19:03:33 -0500

    New

---
+++ archive/weatherAPI/weather.js
@@ -0,0 +1,40 @@
+// Yosemite National Park, California - HALF DOME
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/37.7459,-119.5332"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=37.7459&lon=-119.5332&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Zion National Park, Utah - ANGELS LANDING
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/37.2690,-112.9469"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=37.2690&lon=-112.9469&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+
+// Zion National Park, Utah - THE NARROWS
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/37.3050,-112.9491"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=37.3050&lon=-112.9491&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Wrangell-St. Elias National Park, Alaska - ROOT GLACIER TRAIL
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/61.4877,-142.8913"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=61.4877&lon=-142.8913&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Glacier National Park, Montana - HIGHLINE TRAIL
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/48.7443,-113.7377"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=48.7443&lon=-113.7377&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Canyonlands National Park, Utah - DRUID ARCH TRAIL
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/38.0885,-109.8317"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=38.0885&lon=-109.8317&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Franconia Notch State Park, New Hampshire - FRANCONIA RIDGE LOOP TRAIL
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/44.1608,-71.6447"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=44.1608&lon=-71.6447&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Grand Teton National Park, Wyoming - CASCADE CANYON
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/43.7652,-110.7447"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=43.7652&lon=-110.7447&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Napali Coast, Kanuai, Hawaii - KALALAU TRAIL
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/22.2205,-159.5830"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=22.2205&lon=-159.5830&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
+
+// Maroon Bells, Colorado - Maroon Creek Trail
+DARKSKY - GET "https://api.darksky.net/forecast/0b132d8081de2559178f1ea9126014dc/39.0708,-106.9890"
+WEATHERBIT - GET "https://api.weatherbit.io/v1.0/current?lat=39.0708&lon=-106.9890&units=I&key=fc2cd64b9bb34cf0b7b262c4d7f1d053"
--- server.js
+++ server.js
@@ -1,79 +1,148 @@
-// =================================================================
-// Dependencies
-// =================================================================
-var express = require('express');
-var path = require('path');
-var methodOverride = require('method-override');
+// // =================================================================
+// // Dependencies
+// // =================================================================
+// var express = require('express');
+// var path = require('path');
+// var methodOverride = require('method-override');
 
-// =================================================================
-// Initialize new Express app
-// =================================================================
+// // =================================================================
+// // Initialize new Express app
+// // =================================================================
+// var app = express();
+
+// // =================================================================
+// // View engine setup
+// // =================================================================
+// var exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+
+// // =================================================================
+// // Configure page favicon and public web folder
+// // =================================================================
+// var favicon = require('serve-favicon');
+// app.use(favicon(path.join(__dirname, '/assets/favicon/', 'favicon.ico')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// // =================================================================
+// // Configure body-parser middleware
+// // =================================================================
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// // =================================================================
+// // Setup console logger
+// // =================================================================
+// var logger = require('morgan');
+// app.use(logger('dev'));
+
+// // =================================================================
+// // Configure application routes
+// // =================================================================
+// // app.use(methodOverride('_method'));
+
+// // var controller = require('./controllers/initialcontroller');
+// // app.use('/', controller);
+
+// // // Catch 404 errors, forward to error handlers below.
+// // app.use(function(req, res, next) {
+// //     var err = new Error('Not Found');
+// //     err.status = 404;
+// //     next(err);
+// // });
+
+// // =================================================================
+// // Error handler - development error handler will print stacktrace
+// // =================================================================
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
+
+// // =================================================================
+// // Error handler - production handler not leaking stacktrace to user
+// // =================================================================
+// app.use(function(err, req, res) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
+
+
+
+// module.exports = app;
+
+// RANDOM -- TRYING OUT STUFF
+// RANDOM -- TRYING OUT STUFF
+// RANDOM -- TRYING OUT STUFF
+// RANDOM -- TRYING OUT STUFF
+// RANDOM -- TRYING OUT STUFF
+// RANDOM -- TRYING OUT STUFF
+
+
+// Dependencies
+var express = require("express");
+// var exphbs = require("express-handlebars");
+// var prompt = require("prompt");
+// var inquirer = require("inquirer");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Create an instance of the express app.
 var app = express();
 
-// =================================================================
-// View engine setup
-// =================================================================
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Specify the port.
+var PORT = 4000;
 
-// =================================================================
-// Configure page favicon and public web folder
-// =================================================================
-var favicon = require('serve-favicon');
-app.use(favicon(path.join(__dirname, 'public/assets/img', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// =================================================================
-// Configure body-parser middleware
-// =================================================================
-var bodyParser = require('body-parser');
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// =================================================================
-// Setup console logger
-// =================================================================
-var logger = require('morgan');
-app.use(logger('dev'));
+// ================================================================================
+// ROUTER 
+// require("./app/routing/apiRoutes")(app);
+require("./routing/htmlRoutes.js")(app);
+// ================================================================================
 
-// =================================================================
-// Configure application routes
-// =================================================================
-app.use(methodOverride('_method'));
+// Set Handlebars as the default templating engine.
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
-var controller = require('./controllers/initialcontroller');
-app.use('/', controller);
+// var newFriends = [
+//     { name: "Kristen", hobby: "musician", interestedIn: "Sky diving" },
+// ];
 
-// Catch 404 errors, forward to error handlers below.
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// Routes
+// app.get("/icecreams/:name", function(req, res) {
+//     res.render("/views/layouts/index.handlebars", icecreams[0]);
+// });
+
+// app.get(function(req, res) {
+//     res.sendFile(path.join(__dirname, "/../public/index.html"));
+// });
+
+// app.get("/weekend", function(req, res) {
+//     res.render("index", lunches[1]);
+// });
+
+// app.get("/icecreams/:name", function(req, res) {
+//     res.render("all-icecreams", {
+//         Icecream_List: icecreams,
+//     });
+// });
+
+app.listen(process.env.PORT, function() {
+    console.log('App listening on PORT ' + PORT);
 });
-
-// =================================================================
-// Error handler - development error handler will print stacktrace
-// =================================================================
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// =================================================================
-// Error handler - production handler not leaking stacktrace to user
-// =================================================================
-app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-module.exports = app;
